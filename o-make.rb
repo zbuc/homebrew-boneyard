c45174cd4423edd23a9a750435a7bfeb81f0366b
Jaeho Shin <netj@sparcs.org>
2012-12-12 02:55:12 -0800

    omake 0.9.8.6-0.rc1
    Fixed o-make 0.9.8.6-0.rc1 to build with ocaml 4
    
    Closes #12.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- o-make.rb
+++ o-make.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class OMake < Formula
-  url 'http://omake.metaprl.org/downloads/omake-0.9.8.5-3.tar.gz'
+  url 'http://omake.metaprl.org/downloads/omake-0.9.8.6-0.rc1.tar.gz'
   homepage 'http://omake.metaprl.org/'
-  sha1 'e78c9d8858749f5e234cba303637980e24085cbc'
+  sha1 'f714fa992bf391b8bac49a488bc654128962d1cc'
 
   depends_on 'objective-caml'
 
   def patches
-    # removes reference to missing caml_sync in OS X OCaml
+    # skip all warnings that can happen with newer versions of ocamlc
     DATA
   end
 
@@ -18,19 +18,6 @@ class OMake < Formula
 end
 
 __END__
-diff --git a/src/exec/omake_exec.ml b/src/exec/omake_exec.ml
-index 8c034b5..7e40b35 100644
---- a/src/exec/omake_exec.ml
-+++ b/src/exec/omake_exec.ml
-@@ -46,8 +46,6 @@ open Omake_exec_notify
- open Omake_options
- open Omake_command_type
- 
--external sync : unit -> unit = "caml_sync"
--
- module Exec =
- struct
-    (*
 diff --git a/OMakefile b/OMakefile
 index 9b77a25..1d61d70 100644
 --- a/OMakefile
@@ -40,7 +27,7 @@ index 9b77a25..1d61d70 100644
  # OCaml options
  #
 -OCAMLFLAGS[] += -w Ae$(if $(OCAML_ACCEPTS_Z_WARNING), z)
-+OCAMLFLAGS[] += -w Ae$(if $(OCAML_ACCEPTS_Z_WARNING), z)-9-27..29
++OCAMLFLAGS[] += -warn-error -a
  if $(THREADS_ENABLED)
      OCAMLFLAGS += -thread
      export

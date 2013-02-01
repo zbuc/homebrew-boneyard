a7e8251d47be4d55c038b36c302089e18bd6a1c1
Randy Thiemann <uselinuxnow@gmail.com>
2013-02-01 01:20:27 +0100

    woof: replace md5 with sha1
    Closes #13.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- woof.rb
+++ woof.rb
@@ -3,6 +3,6 @@ require 'formula'
 class Woof < ScriptFileFormula
   url "http://www.home.unix-ag.org/simon/woof"
   version '20091227'
-  md5 '4df770eedef7b011fc37d42015c801b9'
+  sha1 '5070c95601ceefb358af534f2d8ec86d80a28f46'
   homepage 'http://www.home.unix-ag.org/simon/woof.html'
 end

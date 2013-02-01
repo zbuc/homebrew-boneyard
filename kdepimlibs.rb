17aabaf15b9d9e84cfaeff8da2d9721274e23729
Randy Thiemann <uselinuxnow@gmail.com>
2013-02-01 01:11:21 +0100

    kdepimlibs: replace md5 with sha1
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- kdepimlibs.rb
+++ kdepimlibs.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Kdepimlibs < Formula
   homepage 'http://www.kde.org/'
   url 'ftp://ftp.kde.org/pub/kde/stable/4.6.0/src/kdepimlibs-4.6.0.tar.bz2'
-  md5 'bfcc74fff5c6d0803d43cf13033660ab'
+  sha1 '14624960410d270077df530d70d34e270260b3c4'
 
   depends_on 'cmake' => :build
   depends_on 'gpgme'

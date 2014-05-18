2f6e27ca21ae0c7c07530bc0abd8639bb67b0964
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-05-18 12:42:42 +0200

    Fix OMake build on Mavericks
    Fix dependency on readline
    
    This commit fixes omake's building on my system. I do this with a lot of
    assumptions, which I explain below.
    
    OMake fails building because it tries to use GNU readline instead of BSD
    readline.
    * Autodetection tests for `rl_on_new_line` to determine which readline it
      is linking against. But `otool -t -v /usr/lib/libreadline.dylib` shows
      that this function is now defined by the system BSD readline. So it
      mistakenly detects that a GNU readline is available.
    * Then, it tries to use function history_list from GNU libhistory and
      fails.
    
    To fix this, we need to:
    
    * also link `libhistory` in. I assume that libhistory was part of
      libreadline at some point, so I modified the detection routines to
      also link libhistory when detecting GNU's readline library.
    * Have readline available at all during linking. Adding a dependency
      appears sufficient for the job.
    * fix detection of GNU readline. Simply testing for history_list should
      do the job; I know that because linking OMake failed on *that*
      function. This will ensure that software using OMake correctly detects
      which readline library is available.
    
    Note: On my system I do have GNU libreadline installed, but I don't
    think that's what broke the build. The last confirmed build of this
    library is from June 2013, before the release of OS X Mavericks (which I
    use), so I suppose that rl_on_new_line was not part of OS X's
    `libreadline` yet.
    
    I've added a link to the upstream report.
    
    Also, specify version in build. Autodetection fails here, leaving us
    with version '1'.
    
    Closes #22.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- o-make.rb
+++ o-make.rb
@@ -4,11 +4,15 @@ class OMake < Formula
   url 'http://omake.metaprl.org/downloads/omake-0.9.8.6-0.rc1.tar.gz'
   homepage 'http://omake.metaprl.org/'
   sha1 'f714fa992bf391b8bac49a488bc654128962d1cc'
+  version "0.9.8.6"
 
+  depends_on 'readline'
   depends_on 'objective-caml'
 
   def patches
-    # skip all warnings that can happen with newer versions of ocamlc
+    # skip all warnings that can happen with newer versions of ocamlc. Also,
+    # work around a faulty readline detection, see
+    # http://bugzilla.metaprl.org/cgi-bin/show_bug.cgi?id=740.
     DATA
   end
 
@@ -31,3 +35,19 @@ index 9b77a25..1d61d70 100644
  if $(THREADS_ENABLED)
      OCAMLFLAGS += -thread
      export
+diff --git a/lib/configure/readline.om b/lib/configure/readline.om
+index 39c2953..e5d18e6 100644
+--- a/lib/configure/readline.om
++++ b/lib/configure/readline.om
+@@ -39,9 +39,10 @@ open configure/ncurses
+             READLINE_CFLAGS += -DREADLINE_ENABLED
+
+             # Test for GNU
+-            if $(CheckLib ncurses readline, rl_on_new_line)
++            if $(CheckLib ncurses readline history, history_list)
+                 READLINE_GNU = true
+                 READLINE_CFLAGS += -DREADLINE_GNU
++                READLINE_CLIBS += -lhistory
+                 export
+
+             export
